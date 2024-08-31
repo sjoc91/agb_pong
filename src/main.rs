a265043391d6d881750a602aa287f9f0db092685
sjoc91 <sean@DESKTOP-6GCUJAT>
2024-08-30 19:42:13 -0500

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,37 @@
 #![cfg_attr(test, reexport_test_harness_main = "test_main")]
 #![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
 
+use agb::{
+    include_aseprite,
+    display::object::{Graphics, Tag}
+};
+// Import the sprites in to this static. This holds the sprite 
+// and palette data in a way that is manageable by agb.
+static GRAPHICS: &Graphics = include_aseprite!("gfx/sprites.aseprite");
+
+// We define some easy ways of referencing the sprites
+//static PADDLE_END: &Tag = GRAPHICS.tags().get("Paddle End");
+//static PADDLE_MID: &Tag = GRAPHICS.tags().get("Paddle Mid");
+static BALL: &Tag = GRAPHICS.tags().get("Ball");
+
 // The main function must take 1 arguments and never return. The agb::entry decorator
 // ensures that everything is in order. `agb` will call this after setting up the stack
 // and interrupt handlers correctly. It will also handle creating the `Gba` struct for you.
 #[agb::entry]
 fn main(mut gba: agb::Gba) -> ! {
-    agb::no_game(gba);
+    // Get the object manager
+    let object = gba.display.object.get_managed();
+
+    // Create an object with the ball sprite
+    let mut ball = object.object_sprite(BALL.sprite(0));
+
+    // Place this at some point on the screen, (50, 50) for example
+    ball.set_x(50).set_y(50).show();
+
+    // Now commit the object controller so this change is reflected on the screen.
+    // This isn't how we will do this in the final version of the code, but will do
+    // for this example.
+    object.commit();
+    
+    loop {}
 }
